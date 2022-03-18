097e720893528589e7401a77222e782cd35f7828
kianoshm <41006048+kianoshm@users.noreply.github.com>
2022-03-18 09:39:07 +0100

    Add files via upload

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,1 +1,30 @@
-test
+#ECR
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name = var.aws_ecr_repository
+
+  image_scanning_configuration {
+    scan_on_push = var.ecr_scan_on_push
+  }
+  tags = {
+    Name        = var.tag_function.Name
+    Environment = var.tag_function.Environment
+    Application = var.tag_function.Application
+    Section     = var.tag_function.Section
+    Service     = var.tag_function.Service 
+  }
+}
+# ECR Policy
+
+resource "aws_ecr_repository_policy" "ecr_policy" {
+  repository = var.aws_ecr_repository 
+  policy = file("${var.policies_file_path}/ecr_policies/${var.ecr_policy}")
+
+  tags = {
+    Name        = var.tag_function.Name
+    Environment = var.tag_function.Environment
+    Application = var.tag_function.Application
+    Section     = var.tag_function.Section
+    Service     = var.tag_function.Service 
+  }
+}
